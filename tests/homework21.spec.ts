da13f04d0af0908a596848f1845e479c36f8e7f8
Kate22009 <vorovskayak@gmail.com>
2024-02-25 23:02:46 +0200

    homework 21 - test2

--- tests/homework21.spec.ts
+++ tests/homework21.spec.ts
@@ -17,13 +17,24 @@ test("Multi tabbing", async ({ page, context }) => {
   await newTab.press("#sb_form_q", "Enter");
 });
 
-test("Multiple browsers", async ({ page, context }) => {
-  const browser1 = await chromium.launch();
+test("Multiple browsers", async ({}) => {
+  let browser1, browser2;
+  browser1 = await chromium.launch();
   const newPage1 = await browser1.newPage();
-  await newPage1.goto("https://www.google.com");
-  const browser2 = await firefox.launch();
+  browser2 = await firefox.launch();
   const newPage2 = await browser2.newPage();
+
+  await newPage1.goto("https://www.bing.com/");
+
   await newPage2.goto("https://makeup.com.ua/");
+  await newPage2.locator("//div[@class='search-button']").click();
+  await newPage2.fill("//input[@id='search-input']", "Loreal");
+  await newPage2.press("//input[@id='search-input']", "Enter");
+  expect(newPage2.url()).toBe("https://makeup.com.ua/search/?q=loreal");
+
+  await newPage1.fill("#sb_form_q", "Automation testing"); // you an search any other text here
+  await newPage1.press("#sb_form_q", "Enter");
+
   await browser1.close();
   await browser2.close();
 });
